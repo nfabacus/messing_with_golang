f2ca5531634adce3380941b1c5589b2d70f98da9
nfabacus <n.fujioka@abacusmaths.info>
2020-06-03 00:17:14 +0100

    2nd June string type

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 )
 
 type hotdog int
@@ -26,6 +27,43 @@ func main() {
 	y = int(x)
 	fmt.Println(y)
 	fmt.Printf("%T", y)
+
+	fmt.Println("\n##################")
+
+	value1 := 42
+	value2 := 24.356
+	fmt.Println(value1)
+	fmt.Printf("%T\n", value1)
+	fmt.Println(value2)
+	fmt.Printf("%T\n", value2)
+
+	fmt.Println("\n##################")
+	fmt.Println(runtime.GOOS)
+	fmt.Println(runtime.GOARCH)
+
+	fmt.Println("\n##################")
+	s := "Hello, world!!!"
+	s2 := `Yo,
+		friends!
+	`
+	s = "Hi, Bob!"
+	fmt.Println(s)
+	fmt.Printf("%T\n", s)
+	fmt.Println(s2)
+	fmt.Printf("%T\n", s2)
+
+	bs := []byte(s) // convert string to bytestring
+
+	fmt.Println(bs)
+	fmt.Printf("%T\n", bs)
+
+	fmt.Println("\n##################")
+	fmt.Println(s)
+	for i := 0; i < len(s); i++ {
+		//fmt.Println(s[i])
+		fmt.Printf("%#U ", s[i])
+	}
+
 }
 
 func foo() {
